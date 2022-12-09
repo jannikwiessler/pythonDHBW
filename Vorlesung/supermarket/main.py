f4427d8049ddec3fcddb67cfa9ddab3efd9803b0
Jannik Wiessler <jannik.wiessler@gmail.com>
2022-12-09 17:43:28 +0100

    move weather files into separate folder

--- Vorlesung/supermarket/main.py
+++ Vorlesung/supermarket/main.py
@@ -6,24 +6,24 @@ pd.set_option('display.max_columns', 10)
 df = pd.read_csv("supermarket_sales.csv")
 
 # explore the data
-# print(df.columns)
-# print(df.head())
+print(df.columns)
+print(df.head())
 
 # count number of rows
-# print(df.count())
-# print(df["Invoice ID"].count())
+print(df.count())
+print(df["Invoice ID"].count())
 
 # mean of all numerical columns
-# print(df["Total"].mean())
+print(df["Total"].mean())
 
 # count sales per supermaket branch (A,B,C)
-# print(df.groupby("Branch")["Invoice ID"].count())
+print(df.groupby("Branch")["Invoice ID"].count())
 
 # count sales records for gender in each supermarket branch
-# print(df.groupby(["Branch", "Gender"])["Invoice ID"].count())
+print(df.groupby(["Branch", "Gender"])["Invoice ID"].count())
 
 # Filter for Supermaket A
-# print(df[df["Branch"] == "A"])
+print(df[df["Branch"] == "A"])
 
 # number of sales records grouped by product line for each gener in supermaket B
 print(df[df["Branch"] == "B"].groupby(["Product line", "Gender"])["Invoice ID"].count())
